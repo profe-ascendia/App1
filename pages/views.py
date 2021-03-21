aa338e13d7b39dbc8248ddce6d0c77e7238d32d0
Teo <ascendia.teosanchez@gmail.com>
2021-03-21 20:02:38 +0100

    Modificado Hola Mundo

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-    return HttpResponse('Hola mundo')
-    
+    return HttpResponse('<h1>Hola mundo</h1>')
+
